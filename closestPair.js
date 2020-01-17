b9978e98f553082120555f739fd124eba3ef2c75
Michael Cooper <mcooper628@gmail.com>
2020-01-17 10:02:06 -0800

    fixing bug

--- closestPair.js
+++ closestPair.js
@@ -11,23 +11,26 @@ function closestPair(x, y) {
 
   m = createPartition(x)[0];
 
-  let middleArr = []; 
+  let middleArr = [];
 
-  for(let i =0; i<x.length;i++){
-    if(y[i] - x[i]<d){
-      middleArr.push({y:y[i],x:x[i]}); 
+  for (let i = 0; i < x.length; i++) {
+    if (y[i] - x[i] < d) {
+      middleArr.push({ y: y[i], x: x[i] });
     }
   }
 
-  dminsq = Math.pow(d,2);  
+  dminsq = Math.pow(d, 2);
 
-  for(let i = 0; i<x.length-2){
-    let k = i + 1; 
-    while(k<= x.length -1 && middleArr[k].y - middleArr[i].x < dminsq){
-    dminsq = Math.min(Math.pow((middleArr[k].x-middleArr[i].x),2)-Math.pow((middleArr[k].y-middleArr[i].y),2)); 
-    k = k+1; 
+  for (let i = 0; i < x.length - 2; i++) {
+    let k = i + 1;
+    while (k <= x.length - 1 && middleArr[k].y - middleArr[i].x < dminsq) {
+      dminsq = Math.min(
+        Math.pow(middleArr[k].x - middleArr[i].x, 2) -
+          Math.pow(middleArr[k].y - middleArr[i].y, 2)
+      );
+      k = k + 1;
     }
-    return dminsq
+    return dminsq;
   }
 
   console.log(xl, yl, xr, yr);
